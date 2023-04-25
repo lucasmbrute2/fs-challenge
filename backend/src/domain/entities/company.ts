5c7ed61b2ad38f0814878bbb9daa31a7d1e1c107
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-25 20:45:09 -0300

    feat: ensure vite config will be sync with ts modules path

--- backend/src/domain/entities/company.ts
+++ backend/src/domain/entities/company.ts
@@ -1,3 +1,21 @@
-export class Company {
+import { randomUUID } from "node:crypto"
 
+interface CompanyProps {
+  id: string
+  cnpj: string
+  name: string
+  email: string
+  phone: string
+  address: string
+}
+
+export class Company {
+  private props: CompanyProps
+
+  constructor(props: CompanyProps) {
+    this.props = {
+      ...props,
+      id: this.props.id ?? randomUUID()
+    }
+  }
 }
