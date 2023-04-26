f417af34e9d8e11ed5749db1a60304264d6f90a2
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-26 15:40:02 -0300

    feat: add company controller factory

---
+++ backend/src/main/factories/add-company-controller.ts
@@ -0,0 +1,10 @@
+import { DbAddCompanyUseCase } from "@/application/use-cases/add-company/db-add-company";
+import { CompanyRepository } from "@/infra/db/company-repository";
+import { AddCompanyController } from "@/presentation/controllers/add-company/add-company";
+import { Controller } from "@/presentation/protocols/controller";
+
+export const makeAddCompanyController = (): Controller => {
+  const companyRepository = new CompanyRepository()
+  const addCompanyUseCase = new DbAddCompanyUseCase(companyRepository)
+  return new AddCompanyController(addCompanyUseCase)
+}
