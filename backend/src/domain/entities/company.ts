026b9eff75703d289a7fea8c3641d604821dcce7
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-25 23:27:56 -0300

    feat: add Company entity

--- backend/src/domain/entities/company.ts
+++ backend/src/domain/entities/company.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from "node:crypto"
 
-interface CompanyProps {
+export interface CompanyProps {
   id: string
   cnpj: string
   name: string
@@ -10,12 +10,50 @@ interface CompanyProps {
 }
 
 export class Company {
-  private props: CompanyProps
-
-  constructor(props: CompanyProps) {
+  constructor(private props: CompanyProps) {
     this.props = {
       ...props,
       id: this.props.id ?? randomUUID()
     }
+  }
+
+  get id(): string {
+    return this.props.id
+  }
+
+  set cnpj(cnpj: string) {
+    this.props.cnpj = cnpj
+  }
+
+  get cnpj(): string {
+    return this.props.cnpj
+  }
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+  set email(email: string) {
+    this.props.email = email
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+  set phone(phone: string) {
+    this.props.phone = phone
+  }
+
+  get phone(): string {
+    return this.props.phone
+  }
+  set address(address: string) {
+    this.props.address = address
+  }
+
+  get address(): string {
+    return this.props.address
   }
 }
