f0f22840ca15e6dcb4270debb250ad08a454df14
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-26 15:39:43 -0300

    feat: add express route adapter

---
+++ backend/src/main/adapters/express-routes-adapter.ts
@@ -0,0 +1,19 @@
+import { Controller } from "@/presentation/protocols/controller";
+import { httpRequest } from "@/presentation/protocols/http";
+import { Request, Response } from "express";
+
+export const adapRoute = (controller: Controller) => {
+  return async (req: Request, res: Response): Promise<Response> => {
+    const httpRequest: httpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode === 201) {
+      return res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      return res.status(httpResponse.statusCode).json({
+        message: httpResponse.body
+      })
+    }
+  }
+}
