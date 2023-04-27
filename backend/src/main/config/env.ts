e5afb6b0ab488a98a4bb171783013775ec71caa3
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-26 21:51:44 -0300

    feat: setup environment variables

---
+++ backend/src/main/config/env.ts
@@ -0,0 +1,16 @@
+import { ServerError } from '@/presentation/errors/server-error'
+import 'dotenv/config'
+import { z } from "zod"
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+if (_env.success === false) {
+  console.error("Invalid environment variables", _env.error.format())
+  throw new ServerError(_env.error.stack)
+}
+
+export const env = _env.data
