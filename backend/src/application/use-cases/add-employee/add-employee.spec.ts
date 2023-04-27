17b274acba22524f26d6a2514fc5919c947ee662
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-27 11:48:55 -0300

    test: ensure that calls AddEmployeeRepository with correct values

---
+++ backend/src/application/use-cases/add-employee/add-employee.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it, vi } from "vitest";
+import { DbAddEmployeeUseCase } from "./add-employee";
+import { makeAddEmployeeRepository, makeEmployeeModel } from "@/application/tests/factories";
+import { AddEmployeeRepository } from "@/application/protocols/add-employee-repository";
+import { Employee } from "@/domain/entities/employee";
+
+interface SutTypes {
+  sut: DbAddEmployeeUseCase
+  addEmployeeStub: AddEmployeeRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addEmployeeStub = makeAddEmployeeRepository()
+  const sut = new DbAddEmployeeUseCase(addEmployeeStub)
+
+  return {
+    sut,
+    addEmployeeStub
+  }
+}
+
+describe("DbAddEmployee Use Case", () => {
+  it("Should call AddEmployeeRepository with correct values", async () => {
+    const { addEmployeeStub, sut } = makeSut()
+    const addSpy = vi.spyOn(addEmployeeStub, 'add')
+    const response = await sut.add(makeEmployeeModel())
+
+    const employee = new Employee({ ...makeEmployeeModel(), id: response.id })
+    expect(addSpy).toHaveBeenCalledWith(employee)
+  })
+})
