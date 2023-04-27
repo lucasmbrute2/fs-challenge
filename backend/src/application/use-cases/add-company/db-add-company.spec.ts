1099f93c0a646add898f77a0d9f3a1c53fa6f1df
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-27 10:23:31 -0300

    test: ensure that AddCompany returns null if already exists a created company with same credentials

--- backend/src/application/use-cases/add-company/db-add-company.spec.ts
+++ backend/src/application/use-cases/add-company/db-add-company.spec.ts
@@ -2,20 +2,25 @@ import { AddCompanyRepository } from "@/application/protocols/add-company-reposi
 import { Company } from "@/domain/entities/company";
 import { describe, expect, it, vi } from "vitest";
 import { DbAddCompanyUseCase } from "./db-add-company";
-import { makeAddCompanyRepository, makeCompanyModel } from "@/application/tests/factories";
+import { makeAddCompanyRepository, makeCompanyModel, makeFindCompanyByCnpjRepository } from "@/application/tests/factories";
+import { FindCompanyRepository } from "@/application/protocols/find-company-repository";
+import { makeCompany } from "@/domain/entities/tests/factories";
 
 interface SutTypes {
   sut: DbAddCompanyUseCase
   addCompanyRepositoryStub: AddCompanyRepository
+  findCompanyByCnpjStub: FindCompanyRepository
 }
 
 const makeSut = (): SutTypes => {
   const addCompanyRepositoryStub = makeAddCompanyRepository()
-  const dbAddCompanyUseCase = new DbAddCompanyUseCase(addCompanyRepositoryStub)
+  const findCompanyByCnpjStub = makeFindCompanyByCnpjRepository()
+  const dbAddCompanyUseCase = new DbAddCompanyUseCase(addCompanyRepositoryStub, findCompanyByCnpjStub)
 
   return {
     sut: dbAddCompanyUseCase,
-    addCompanyRepositoryStub
+    addCompanyRepositoryStub,
+    findCompanyByCnpjStub
   }
 }
 
@@ -25,7 +30,7 @@ describe("DbAddCompany Use Case", () => {
     const addSpy = vi.spyOn(addCompanyRepositoryStub, 'add')
     const companyResponse = await sut.add(makeCompanyModel())
 
-    const company = new Company({ ...makeCompanyModel(), id: companyResponse.id })
+    const company = new Company({ ...makeCompanyModel(), id: companyResponse?.id })
     expect(addSpy).toHaveBeenCalledWith(company)
   })
 
@@ -33,7 +38,15 @@ describe("DbAddCompany Use Case", () => {
     const { sut } = makeSut()
     const company = await sut.add(makeCompanyModel())
 
-    expect(company).toEqual(new Company({ ...makeCompanyModel(), id: company.id }))
+    expect(company).toEqual(new Company({ ...makeCompanyModel(), id: company?.id }))
     expect(company).toBeInstanceOf(Company)
+  })
+
+  it("Should return 400 if Company is already added", async () => {
+    const { sut, findCompanyByCnpjStub } = makeSut()
+    vi.spyOn(findCompanyByCnpjStub, 'find').mockReturnValueOnce(Promise.resolve(makeCompany()))
+    const response = await sut.add(makeCompanyModel())
+
+    expect(response).toBeFalsy()
   })
 })
